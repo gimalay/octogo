4d2326733870fa0b6338ade823fe697e0d0ebce7
Dmytro Halichenko <dmytro.halichenko@gmail.com>
2020-05-13 16:26:18 -0400

    task and project protobuf models

--- todoapp/codegen.sh
+++ todoapp/codegen.sh
@@ -1,13 +1,14 @@
 cd "$GOPATH"/src || exit
-protoc --go_out=plugins=grpc:. -I "$GOPATH"/src github.com/gimalay/octogo/todoapp/model/project.proto
-protoc --go_out=plugins=grpc:. -I "$GOPATH"/src github.com/gimalay/octogo/todoapp/model/task.proto
-protoc --go_out=plugins=grpc:. -I "$GOPATH"/src github.com/gimalay/octogo/todoapp/model/event.proto
-protoc --go_out=plugins=grpc:. -I "$GOPATH"/src github.com/gimalay/octogo/todoapp/app/command.proto
-protoc --go_out=plugins=grpc:. -I "$GOPATH"/src github.com/gimalay/octogo/todoapp/app/viewModel.proto
+gofiles=("aggregate/events" "aggregate/project")
 
-cd "$GOPATH"/src/github.com/gimalay/octogo/todoapp/app || exit
-protoc --swift_out=../ios/todoapp/src/Models command.proto
-protoc --swift_out=../ios/todoapp/src/Models viewModel.proto
+for f in "${gofiles[@]}"
+do
+protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gofast_out=\
+Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:. \
+github.com/gimalay/octogo/todoapp/core/$f.proto
+done
 
-protoc --java_out=../android/todoapp/app/src/main/java viewModel.proto
-protoc --java_out=../android/todoapp/app/src/main/java command.proto
